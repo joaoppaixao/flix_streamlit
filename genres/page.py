9041c1c0aa5b7bfe007fb07b64f21b62da90ad31
joao <dev@monitoo.com.br>
2026-04-27 15:47:12 -0300

    feat: finalizando a tela de genres

--- genres/page.py
+++ genres/page.py
@@ -1,33 +1,33 @@
 import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid
+from genres.service import GenreService
 
-genres = [
-    {
-        'id': 1,
-        'name': 'Ação'
-    },
-    {
-        'id': 2,
-        'name': 'Comédia'
-    },
-    {
-        'id': 3,
-        'name': 'Terror'
-    },
-]
 
 def show_genres():
-    st.write('Lista de Gêneros:')
+    genre_service =  GenreService()
+    genres = genre_service.get_genres()
 
-    AgGrid(
-        data=pd.DataFrame(genres),
-        reload_data=True,
-        enableSorting=True,
-        key='genres_grid',
-    )
+    if genres:
+        st.write('Lista de Gêneros:')
+        genres_df = pd.json_normalize(genres)
+        AgGrid(
+            data=genres_df,
+            reload_data=True,
+            enableSorting=True,
+            key='genres_grid',
+        )
+    else:
+        st.warning('Nenhum gênero enconstrado. ')
+
 
     st.title('Cadastrar novo Gênero')
     name = st.text_input('Nome do Gênero')
     if st.button('Cadastrar'):
-        st.success(f'Gênero "{name}" cadastrado com sucesso!')
+        new_genre = genre_service.create_genre(
+            name=name,
+        )
+        if new_genre:
+            st.rerun()
+        else:
+            st.error('Erro ao cadastrar o gênero. Verifique os campos. ')
