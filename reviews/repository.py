ffcb08e8c2d0fb773b61d718f226e2453378c574
joao <dev@monitoo.com.br>
2026-05-06 10:38:06 -0300

    feat: implemtando a page.py de reviews e ajustando repository

--- reviews/repository.py
+++ reviews/repository.py
@@ -24,14 +24,15 @@ class ReviewRepository():
             return None
         raise Exception(f'Erro ao obter dados da API. Status code: {response.status_code}')
     
-    def create_review(self):
+    def create_review(self, review):
         response = requests.post(
             self.__reviews_url,
-            headers=self.__headers
+            headers=self.__headers,
+            data=review,
         )
-        if response.status_code == 200:
-            return response.json()
         if response.status_code == 201:
+            return response.json()
+        if response.status_code == 401:
             logout()
             return None
         raise Exception(f'Erro ao obter dados da API. Status code: {response.status_code}')
